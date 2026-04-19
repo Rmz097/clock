fe04b56cea153410b6411ecd1b0d54dde4ba7104
José Ramirez <jlramirezc097@gmail.com>
2026-04-18 19:35:17 -0600

    Ajuste tono de voz

--- js/script.js
+++ js/script.js
@@ -1036,8 +1036,8 @@ const minutos = ahora.getMinutes();
 
     if (isAndroidVoice) {
         // 📱 Android → más lento (más natural)
-        utterance.rate = 0.8;
-        utterance.pitch = 1.5;
+        utterance.rate = 0.5;
+        utterance.pitch = 2;
     } else {
         // 💻 PC / Windows → normal
         utterance.rate = 1;
